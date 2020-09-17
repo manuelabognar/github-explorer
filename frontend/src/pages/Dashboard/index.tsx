f29e82b8c99c15629a0b414cda5cf3f9cbee6677
Manuela Bognar <manuelabcamara@gmail.com>
2020-09-17 05:52:43 -0300

    Dashboard: html + styles

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/34453910?s=460&u=7b4f8b500da071b5f404974bb0712c1f9a504e20&v=4"
+            alt="Manuela Bognar"
+          />
+          <div>
+            <strong>manuelabognar/github-explorer</strong>
+            <p>Loren ipsun dolar sit amet</p>
+          </div>
+
+          <FiChevronRight size={20}/>
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/34453910?s=460&u=7b4f8b500da071b5f404974bb0712c1f9a504e20&v=4"
+            alt="Manuela Bognar"
+          />
+          <div>
+            <strong>manuelabognar/github-explorer</strong>
+            <p>Loren ipsun dolar sit amet</p>
+          </div>
+
+          <FiChevronRight size={20}/>
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/34453910?s=460&u=7b4f8b500da071b5f404974bb0712c1f9a504e20&v=4"
+            alt="Manuela Bognar"
+          />
+          <div>
+            <strong>manuelabognar/github-explorer</strong>
+            <p>Loren ipsun dolar sit amet</p>
+          </div>
+
+          <FiChevronRight size={20}/>
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
